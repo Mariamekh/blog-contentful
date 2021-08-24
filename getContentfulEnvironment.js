3ea6710081caa221521b0f02cb9d3049e30e6533
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2021-08-25 00:59:11 +0400

    feat: add contentful and types

---
+++ getContentfulEnvironment.js
@@ -0,0 +1,11 @@
+const contentfulManagement = require("contentful-management")
+
+module.exports = function() {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: 'CFPAT-ub2Cm7fhOpx9KmhGxw4UtcLitxZnJJOWr-XAsXLauSY',
+  })
+
+  return contentfulClient
+    .getSpace('kt8nwgtmnmp4')
+    .then(space => space.getEnvironment('master'))
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,42 @@
 /* eslint-disable @next/next/no-typos */
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import  { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { IHome, IHomeFields } from '../@types/generated/contentful'
+import client from '../contentful'
  
 
-export default function Home({title}: {title:string}){
+export default function Home({home}: {home: IHome}){
+ console.log(home)
   return (
     <div>
       <Head>
-        <title>{title}</title>
+        <title>{home.fields.title}</title>
        </Head>
 
       <main>
-        <h1>{title}</h1>
+        <h1>{home.fields.title}</h1>
+        <div>
+          {documentToReactComponents(home.fields.description!)}
+        </div>
      </main>
     </div>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () =>{
+  const home = await client.getEntries<IHomeFields>({
+    content_type: 'home',
+    limit:1
+  })
+ 
+  const [homePage] = home.items;
+
   return {
     props:{
-      title:'My Blog'
-    }
+       home: homePage,
+    },
+    revalidate: 3600
   }
 }
  
