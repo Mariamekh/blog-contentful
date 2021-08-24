f408970e368da0f39235998a0ab4a5c2628aa781
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2021-08-25 02:18:28 +0400

    feat: implement title and description from contentful

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import  { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { IHome, IHomeFields } from '../@types/generated/contentful'
 import client from '../contentful'
- 
+import {Container, Row, Col} from 'reactstrap'
 
 export default function Home({home}: {home: IHome}){
  console.log(home)
@@ -15,9 +15,17 @@ export default function Home({home}: {home: IHome}){
        </Head>
 
       <main>
-        <h1>{home.fields.title}</h1>
-        <div>
-          {documentToReactComponents(home.fields.description!)}
+        <div
+         className='text-center p-5 text-white' 
+         style={{
+           background:`url("http:${home.fields.background?.fields.file.url}") no-repeat center / cover`,
+           minHeight:600
+        }}
+        >
+          <h1 className='mt-5'>{home.fields.title}</h1>
+          <div className='mt-5'>
+            {documentToReactComponents(home.fields.description!)}
+          </div>
         </div>
      </main>
     </div>
