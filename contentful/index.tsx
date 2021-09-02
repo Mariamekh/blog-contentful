17d6fd10f965ed055a16bbe5b3bf56fe5052352c
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2021-09-02 19:34:17 +0400

    feat: add eslint and prettier

--- contentful/index.tsx
+++ contentful/index.tsx
@@ -1,8 +1,8 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
 
 const client = createClient({
-    space: 'kt8nwgtmnmp4',
-    accessToken: '226mfg3YehabDr61vKq3pRQUAM55a4Z9rT-HUUlJGKk'
-})
+  space: 'kt8nwgtmnmp4',
+  accessToken: '226mfg3YehabDr61vKq3pRQUAM55a4Z9rT-HUUlJGKk'
+});
 
 export default client;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 /* eslint-disable @next/next/link-passhref */
 /* eslint-disable @next/next/no-typos */
-import {
-  documentToReactComponents
-} from '@contentful/rich-text-react-renderer'
-import {
-  GetStaticProps
-} from 'next'
-import Head from 'next/head'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import {
   IArticleFields,
   IArticle,
   IHome,
   IHomeFields
-} from '../@types/generated/contentful'
-import client from '../contentful'
+} from '../@types/generated/contentful';
+import client from '../contentful';
 import {
   Container,
   Row,
@@ -22,73 +18,74 @@ import {
   CardTitle,
   CardText,
   Button
-} from 'reactstrap'
-import Link from 'next/link'
+} from 'reactstrap';
+import Link from 'next/link';
 
+export default function Home({
+  home,
+  articles
+}: {
+  home: IHome;
+  articles: IArticle[];
+}) {
+  return (
+    <div>
+      <Head>
+        <title>{home.fields.title}</title>
+      </Head>
+      <main>
+        <div
+          className="text-center p-5 text-white"
+          style={{
+            background: `url("http:${home.fields.background?.fields.file.url}") no-repeat center / cover`,
+            minHeight: 600
+          }}
+        >
+          <h1 className="mt-5">{home.fields.title}</h1>
+          <div className="mt-5">
+            {documentToReactComponents(home.fields.description!)}
+          </div>
+        </div>
 
-export default function Home({home, articles}: {home: IHome, articles:IArticle[]}){
-    return (
-     <div>
-       <Head>
-         <title>{home.fields.title}</title>
-       </Head>
-       <main>
-          <div
-            className='text-center p-5 text-white' 
-            style={{
-              background:`url("http:${home.fields.background?.fields.file.url}") no-repeat center / cover`,
-              minHeight:600
-              }}
-          >
-            <h1 className='mt-5'>{home.fields.title}</h1>
-            <div className='mt-5'>
-              {documentToReactComponents(home.fields.description!)}
-            </div>
-         </div>
-
-         <Container className="pt-5">
-            <Row> 
-              {articles.map(article =>{
-                return (
-                  <Col sm={4} key={article.fields.slug}>
-                    <Card body>
-                      <CardTitle tag="h5">
-                        {article.fields.title}
-                      </CardTitle>
-                      <CardText>
-                        {article.fields.description}
-                      </CardText>
-                      <Link href={`/articles/${article.fields.slug}`}>
-                          <Button> {article.fields.action}</Button>
-                      </Link>
-                    </Card>
-                  </Col>
-                )
-              })}
-            </Row>
-          </Container>
+        <Container className="pt-5">
+          <Row>
+            {articles.map(article => {
+              return (
+                <Col sm={4} key={article.fields.slug}>
+                  <Card body>
+                    <CardTitle tag="h5">{article.fields.title}</CardTitle>
+                    <CardText>{article.fields.description}</CardText>
+                    <Link href={`/articles/${article.fields.slug}`}>
+                      <Button> {article.fields.action}</Button>
+                    </Link>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
       </main>
-     </div>
-   )
- }
+    </div>
+  );
+}
 
 export const getStaticProps: GetStaticProps = async () => {
-  const home = await client.getEntries < IHomeFields > ({
+  const home = await client.getEntries<IHomeFields>({
     content_type: 'home',
     limit: 1
-  })
+  });
 
-  const article = await client.getEntries < IArticleFields > ({
-    content_type: 'article',
-  })
+  const article = await client.getEntries<IArticleFields>({
+    content_type: 'article'
+  });
 
   const [homePage] = home.items;
 
   return {
     props: {
       home: homePage,
-      articles: article.items,
+      articles: article.items
     },
     revalidate: 3600
-  }
-}
+  };
+};
